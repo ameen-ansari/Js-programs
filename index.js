e9be3afffa0a66fb0441f2f0b610fc9ec3c002bc
M.Ameen <ameen55668@gmail.com>
2022-07-26 15:44:36 +0500

    solve 6 js programs

---
+++ index.js
@@ -0,0 +1,63 @@
+// 1. Write a js program to find maximum between two numbers.
+// let a = 15
+// let b = 25
+// if(a<b){
+//     console.log('b is greater than a ');
+// }else{
+//     console.log('a is greater than a ');
+// }
+
+// 2. Write a js program to find maximum between three numbers.
+// let a = 15
+// let b = 500
+// let c = 35
+// if(a>b && a>c){
+//     console.log('a');
+// }
+// else if(b>a && b>c){
+//     console.log('b');
+// }
+// else if(c>a && c>a){
+//     console.log('b');
+// }
+
+// 3. Write a js program to check whether a number is negative, positive or zero.
+// let a = -15
+// if(a<0){
+//     console.log('negative');
+// }
+// else if(a>0){
+//     console.log('positive');
+// }
+// else if(a=0){
+//     console.log('0');
+// }
+
+// 4. Write a js program to check whether a number is divisible by 5 and 11 or not.
+// let a = 121
+// if(a%5===0){
+//     console.log('use 5');
+// }
+// else if(a%11===0){
+//     console.log('use 11');
+// }
+
+// 5. Write a js program to check whether a number is even or odd.
+// let a = 120
+// if(a%2===0){
+//     console.log('even');
+// }
+// else if(a%2!==0){
+//     console.log('odd');
+// }
+
+// 6. Write a js program to check whether a year is leap year or not.
+// let y = new Date()
+// let year=y.getFullYear()
+// if(year%4 === 0){
+//     console.log('leap year');
+// }
+// else{
+//     console.log('not a leap year');
+// }
+
