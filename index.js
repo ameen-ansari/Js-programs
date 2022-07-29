29c740ee8603aa0de4f91b3518a4d7713cd7e524
M.Ameen <ameen55668@gmail.com>
2022-07-29 11:02:46 +0500

    Solve 16 programs

--- index.js
+++ index.js
@@ -100,4 +100,108 @@
 // }
 
 // 11. Write a js program to input week number and print week day
-var dt = new Date()
+// var today = new Date()
+// var starty = new Date(today.getFullYear(),0)
+// var days = Math.ceil((today-starty)/(1000*60*60*24))
+// console.log(days);
+// var weekno = (days/7)
+// console.log(weekno);
+// var day =['sunday','monday','tuesday','wednesday','thusday','friday','saturday'][new Date().getDay()]
+// console.log(day);
+
+// 12. Write a js program to input month number and print number of days in that month.
+// var today = new Date()
+// console.log(today.getMonth() + 1)
+// var getDaysInMonth = function (month, year) {
+//   return new Date(year, month, 0).getDate()
+// }
+// console.log(getDaysInMonth(2, 2022))
+
+// 13. Write a js program to count total number of notes in given amount.
+// var aarray = [1,2,3,4,56,67,8]
+// console.log(aarray.length);
+
+// 14. Write a js program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+// Percentage >= 90% : Grade A
+// Percentage >= 80% : Grade B
+// Percentage >= 70% : Grade C
+// Percentage >= 60% : Grade D
+// Percentage >= 40% : Grade E
+// Percentage < 40% : Grade F
+// let marks = (72+70+70+69+72)
+// let avgmarks = (marks/5)
+// console.log(avgmarks);
+
+// if(avgmarks >=90 ){
+//   console.log('Grade A');
+// }
+// else if(avgmarks >=80 ){
+//   console.log('Grade B');
+// }
+// else if(avgmarks >=70 ){
+//   console.log('Grade C');
+// }
+// else if(avgmarks >=60 ){
+//   console.log('Grade C');
+// }
+// else if(avgmarks >=40 ){
+//   console.log('Grade D')
+// }
+// else{
+//   console.log('Grade F');
+// }
+
+// 15. Write a js program to input basic salary of an employee and calculate its Gross salary according to following:
+// Basic Salary <= 10000 : HRA = 20%, DA = 80%
+// Basic Salary <= 20000 : HRA = 25%, DA = 90%
+// Basic Salary > 20000 : HRA = 30%, DA = 95%
+// var salary = 120000;
+// if(salary <= 10000){
+//   var HRA = salary*0.20
+//   var DA = salary*0.80
+//  console.log('Total HRA is '+(HRA));
+//  console.log("Total DA is "+(DA));
+//  console.log('Total Salary is ' + (HRA + DA));
+// }
+// else if(salary <= 20000){
+//   var HRA = salary*0.25
+//   var DA = salary*0.90
+//  console.log('Total HRA is '+(HRA));
+//  console.log("Total DA is "+(DA));
+//  console.log('Total Salary is ' + (HRA + DA));
+// }
+// else if(salary > 20000){
+//   var HRA = salary*0.30
+//   var DA = salary*0.95
+//  console.log('Total HRA is '+(HRA));
+//  console.log("Total DA is "+(DA));
+//  console.log('Total Salary is ' + (HRA + DA));
+// }
+
+// 16. Write a js program to input electricity unit charges and calculate total electricity bill according to the given condition:
+// For first 50 units Rs. 0.50/unit
+// For next 100 units Rs. 0.75/unit
+// For next 100 units Rs. 1.20/unit
+// For unit above 250 Rs. 1.50/unit
+// An additional surcharge of 20% is added to the bill
+// var unit = 144
+// var bill=''
+//   if(unit <= 50){
+//     var bill = unit*0.5
+//   }
+//   else if(unit <= 100){
+//     var bill = 50*0.5 + (unit-50)*0.75
+//   }
+//   else if(unit <= 200){
+//     var bill = 50*0.5 + 100*0.75 + (unit-150)*1.20
+//   }
+//   else if(unit <= 250){
+//     var bill = 50*0.5 + 100*0.75 + 100*1.20 +(unit-250)*1.50
+//   }
+//   if(unit > 0){
+//     var bill = bill+bill*0.20
+//   }
+//   console.log(bill);
+
+
+// 25% is completeted
